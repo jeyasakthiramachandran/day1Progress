5c0043b03087f4d91b761fd96d71573208764322
jeyasakthiramachandran <dummy@gmail.com>
2026-04-10 05:11:17 +0000

    solve dummy initial commit

---
+++ server/src/main/java/com/edutech/progressive/entity/Team.java
@@ -0,0 +1,9 @@
+package com.edutech.progressive.entity;
+
+public class Team {
+    private int teamId;
+    private String teamName;
+    private String location;
+    private String ownerName;
+    
+}
