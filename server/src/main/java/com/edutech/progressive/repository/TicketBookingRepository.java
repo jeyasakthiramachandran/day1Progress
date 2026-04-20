d243197cae68348ceb1d45efa7f475a03b960b60
jeyasakthiramachandran <dummy@gmail.com>
2026-04-20 04:12:48 +0000

    day 13 final commit

--- server/src/main/java/com/edutech/progressive/repository/TicketBookingRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/TicketBookingRepository.java
@@ -1,27 +1,36 @@
 package com.edutech.progressive.repository;
-
 import java.util.List;
-
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+ 
 import org.springframework.data.jpa.repository.Modifying;
+ 
 import org.springframework.data.jpa.repository.Query;
+ 
+import org.springframework.data.repository.query.Param;
+ 
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.edutech.progressive.entity.TicketBooking;
 
 @Repository
-public interface TicketBookingRepository extends JpaRepository<TicketBooking, Integer> {
-
+ 
+public interface TicketBookingRepository extends JpaRepository<TicketBooking, Integer>{
+ 
     List<TicketBooking> findByEmail(String email);
-
     @Modifying
+ 
     @Transactional
-    @Query("DELETE FROM TicketBooking tb WHERE tb.match.firstTeam.teamId = ?1 OR tb.match.secondTeam.teamId = ?1")
-    void deleteByTeamId(int teamId);
-
+ 
+    @Query("DELETE FROM TicketBooking t WHERE t.match.matchId IN (SELECT m.matchId FROM matches m WHERE m.firstTeam.teamId = :teamId OR m.secondTeam.teamId = :teamId)")
+ 
+    void deleteByTeamId(@Param("teamId") int teamId);
+ 
     @Modifying
+ 
     @Transactional
-    @Query("DELETE FROM TicketBooking tb WHERE tb.match.matchId = ?1")
-    void deleteByMatchId(int matchId);
+ 
+    @Query("DELETE FROM TicketBooking t WHERE t.match.matchId IN :matchId")
+ 
+    void deleteByMatchId(@Param("matchId") int matchId);
+ 
 }
